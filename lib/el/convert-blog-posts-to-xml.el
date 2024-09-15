72c1f84fd75d4c2fb45f87917d962b302734ced5
ilmari-koria <ilmarikoria@posteo.net>
2024-09-15 14:01:00 +0100

    [el] fix paths

--- lib/el/convert-blog-posts-to-xml.el
+++ lib/el/convert-blog-posts-to-xml.el
@@ -1,5 +1,5 @@
 (require 'package)
-;; (package-initialize)
+(package-initialize)
 ;; (package-refresh-contents)
 
 (unless (package-installed-p 'dash)
@@ -10,13 +10,14 @@
   (package-refresh-contents)
   (package-install 's))
 
+;; TODO is this good practice?
 (add-to-list 'load-path ".")
+
 (require 'org-ml)
 (require 'om-to-xml)
 
-;; TODO change to relative dir
 (defun convert-blog-posts-to-xml ()
-  (let ((directory (concat (getenv "HOME") "/my-files/blog/pages/posts/")))
+  (let ((directory "../../posts/"))
     (when (file-directory-p directory)
       (dolist (file (directory-files directory t "\\.org$"))
         (when (file-regular-p file)
